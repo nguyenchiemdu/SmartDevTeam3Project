42d470a2c6ed62f5c0f63285e76dbafbe9b1aa05
Bin Tran <kurochan159357@gmail.com>
2022-01-19 17:13:28 +0700

    Analyze comment and response comment

--- models/Comment.js
+++ models/Comment.js
@@ -6,6 +6,7 @@ const Comment = new Schema({
     user_id : { type: Schema.Types.ObjectId, ref: 'User' },
     course_id : { type: Schema.Types.ObjectId, ref: 'Course' },
     commentContent: { type: String, required: true},
+    analyzeComment: {type: String, required: true}
 }, {
     timestamps: true,
 });
