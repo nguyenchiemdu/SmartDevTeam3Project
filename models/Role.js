0c637e914334fd0e117c540325dce5f30f8f189a
Bin Tran <kurochan159357@gmail.com>
2022-01-26 19:51:00 +0700

    delete user_id in role schema

--- models/Role.js
+++ models/Role.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Role = new Schema({
-    user_id : { type: Schema.Types.ObjectId, ref: 'User' },
     roleName: { type: String, required: true, },
 }, {
     timestamps: true,
